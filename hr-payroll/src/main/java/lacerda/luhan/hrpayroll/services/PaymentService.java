6a5ac92d6e80ad205bf0481cdb07c5eca2f4e17f
luhanlacerda <luhanlacerda@gmail.com>
2021-03-24 15:17:22 -0300

    Feign client

--- hr-payroll/src/main/java/lacerda/luhan/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/lacerda/luhan/hrpayroll/services/PaymentService.java
@@ -2,29 +2,27 @@ package lacerda.luhan.hrpayroll.services;
 
 import lacerda.luhan.hrpayroll.entities.Payment;
 import lacerda.luhan.hrpayroll.entities.Worker;
+import lacerda.luhan.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workId));
 
-        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        try {
+            Worker worker = workerFeignClient.findById(workId).getBody();
+            Payment payment = worker != null ? new Payment(worker.getName(), worker.getDailyIncome(), days) : null;
+
+            return payment;
+        } catch (Exception e) {
+            //TODO loggar
+            return null;
+        }
     }
 
 }
