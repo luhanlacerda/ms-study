f7a699892d3a6547f7038d01816ee36fbbd3c0b8
luhanlacerda <luhanlacerda@gmail.com>
2021-03-24 14:42:49 -0300

    add RestTemplate in hr-payroll

---
+++ hr-payroll/src/main/java/lacerda/luhan/hrpayroll/entities/Worker.java
@@ -0,0 +1,21 @@
+package lacerda.luhan.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
