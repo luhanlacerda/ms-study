a88dbfeb84c5a28e41ddb3d3acaebb93f6c8ceed
luhanlacerda <luhanlacerda@gmail.com>
2021-03-25 10:03:17 -0300

    Login, JWT token

--- hr-oauth/src/main/java/lacerda/luhan/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/lacerda/luhan/hroauth/services/UserService.java
@@ -3,16 +3,19 @@ package lacerda.luhan.hroauth.services;
 import lacerda.luhan.hroauth.entities.User;
 import lacerda.luhan.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
+    // Apenas didático
     public User findByEmail(String email) {
-
         try {
             User user = userFeignClient.findByEmail(email).getBody();
 
@@ -21,6 +24,20 @@ public class UserService {
             //TODO loggar
             return null;
         }
+    }
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        try {
+            User user = userFeignClient.findByEmail(s).getBody();
+            if (user == null) {
+                throw new UsernameNotFoundException("Email not found");
+            } else {
+                return user;
+            }
+        } catch (Exception e) {
+            //TODO loggar
+            return null;
+        }
     }
 }
