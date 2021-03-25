4fc14e690110e54e6add7fdf5d082760ad3ecea6
luhanlacerda <luhanlacerda@gmail.com>
2021-03-25 08:10:31 -0300

    Configuring hr-user project

---
+++ hr-user/src/test/java/lacerda/luhan/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package lacerda.luhan.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
