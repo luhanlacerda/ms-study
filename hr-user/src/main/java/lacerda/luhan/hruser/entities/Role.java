2ca5765fb5b249510cc68c9d745e0e7567d61efa
luhanlacerda <luhanlacerda@gmail.com>
2021-03-25 08:22:32 -0300

    JPA association to User and Role on hr-user

---
+++ hr-user/src/main/java/lacerda/luhan/hruser/entities/Role.java
@@ -0,0 +1,23 @@
+package lacerda.luhan.hruser.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+}
